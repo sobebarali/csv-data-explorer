96774fe6224504c1f72294612235efb895a04dbc
sobebarali <sobebar_ali17@hotmail.com>
2024-12-05 17:44:46 +0530

    chore: update database connection configuration to use environment variables
    - Removed hardcoded database URL from alembic.ini for security reasons
    - Integrated dotenv to load environment variables from a .env file
    - Set sqlalchemy.url in alembic/env.py to use DATABASE_URL from the environment

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,14 +10,19 @@ from alembic import context
 from database import Base
 from src.modules.csv_module.models import CSVData 
 
+# Load environment variables from .env file
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
+
+# Set the sqlalchemy.url from the environment variable
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
